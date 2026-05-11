3260a80b839ca77a871acc712235d8b83cee8f0f
AnChi0502 <AnChi0502@users.noreply.github.com>
2026-05-11 23:01:50 +0800

    feat: 修改setting.py

--- finalproject/finalproject/settings.py
+++ finalproject/finalproject/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'final_project',
         'USER': 'root',
-        'PASSWORD': '0000',
+        'PASSWORD': '111405062',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
