98e5c0df255435e6187b21f8d1466372469a2f65
LUICEE180 <SANDAR215864@GMAIL.COM>
2026-05-11 23:42:22 +0800

    finish onboarding and resume feature

--- microproject_data_insert.py
+++ microproject_data_insert.py
@@ -5,7 +5,7 @@ DB_CONFIG = {
     'host': '127.0.0.1',
     'port': 3306,
     'user': 'root',
-    'password': '0000',
+    'password': '215864',
     'database': 'final_project',
     'charset': 'utf8mb4'
 }
